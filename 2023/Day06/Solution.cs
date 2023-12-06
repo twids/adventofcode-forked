4a5ac1948c049cd58f2059377ac0083c7a7cf2d3
Tomas Widsell <tomas.widsell@riksdagen.se>
2023-12-06 14:50:27 +0100

    Day06

--- 2023/Day06/Solution.cs
+++ 2023/Day06/Solution.cs
@@ -8,13 +8,36 @@ using System.Text;
 namespace AdventOfCode.Y2023.Day06;
 
 [ProblemName("Wait For It")]
-class Solution : Solver {
+partial class Solution : Solver {
 
     public object PartOne(string input) {
-        return 0;
+        var rows = input.Split("\n");
+        var times = MyRegex().Matches(rows[0]).Select(s => long.Parse(s.Value)).ToArray();
+        var records = MyRegex().Matches(rows[1]).Select(s => long.Parse(s.Value)).ToArray();
+
+        var count = 1;
+        for (var i = 0; i < times.Length; i++)
+            count *= Winning(times, records, i);
+
+        return count;
     }
 
     public object PartTwo(string input) {
-        return 0;
+        var rows = input.Replace(" ", "").Split("\n");
+        var time = MyRegex().Matches(rows[0]).Select(s => long.Parse(s.Value)).First();
+        var record = MyRegex().Matches(rows[1]).Select(s => long.Parse(s.Value)).First();
+
+        return Winning([time], [record], 0);
     }
+
+    private static int Winning(long[] times, long[] records, int i) {
+        var win = 0;
+        for (var velocity = 0; velocity < times[i]; velocity++)
+            if (velocity * (times[i] - velocity) > records[i]) win++;
+
+        return win;
+    }
+
+    [GeneratedRegex(@"\d+")]
+    private static partial Regex MyRegex();
 }
