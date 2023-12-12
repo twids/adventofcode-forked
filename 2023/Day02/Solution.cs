d8ab2c781033b1d1691518eeeb7f2159b29e3388
Tomas Widsell <twidsell@gmail.com>
2023-12-12 13:20:13 +0100

    Init missing days

---
+++ 2023/Day02/Solution.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AdventOfCode.Y2023.Day02;
+
+[ProblemName("Cube Conundrum")]
+class Solution : Solver {
+
+    public object PartOne(string input) {
+        return 0;
+    }
+
+    public object PartTwo(string input) {
+        return 0;
+    }
+}
